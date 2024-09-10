6af7d414bad2e2753fa496f7c915b9de68dd147e
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-11 03:59:42 +0530

    25PetSoftV5=> Heading fixed

--- src/components/branding.tsx
+++ src/components/branding.tsx
@@ -4,7 +4,7 @@ export default function Branding() {
 	return (
 		<section>
 			<H1>
-				Pet<span className="font-semibold">Soft</span>
+				Pet<span className="font-semibold">Care</span>
 			</H1>
 			<p className="text-lg opacity-80">
 				Manage your pet daycare with ease
